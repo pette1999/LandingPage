2d6bccccb3a9d6c871020b4e60d46ef201a224bf
pette1999 <peterchen.eth@gmail.com>
2023-07-11 02:41:19 -0700

    update the landing main

--- Hexlink-Landing/src/components/Main.tsx
+++ Hexlink-Landing/src/components/Main.tsx
@@ -47,10 +47,14 @@ export default function Main() {
     <section>
       <div className="custom-screen pt-[60px] pb-28 flex flex-none items-center flex-col flex-nowrap gap-[80px] h-min justify-center overflow-hidden relative w-full">
         <div className=" duration-1000 delay-300 opacity-1">
-          <div className="max-w-3xl mx-auto text-center flex flex-col flex-nowrap gap-[40px] justify-center items-center overflow-visible min-h-min">
+          <div className="max-w-[800px] mx-auto text-center flex flex-col flex-nowrap gap-[40px] justify-center items-center overflow-visible min-h-min">
             <div className="relative w-auto opacity-100" style={{transform: "perspective(1200px)"}}>
               <div className="contents">
-                <a className="flex flex-row items-center flex-nowrap gap-[8px] min-h-min justify-center overflow-visible py-[8px] pl-[16px] pr-[8px] relative min-w-min border-solid border-x-0 border-y-0 border-transparent bg-white rounded-[40px] opacity-100" href="./design-token-format">
+                <a 
+                  className="flex flex-row items-center flex-nowrap gap-[8px] min-h-min justify-center overflow-visible py-[8px] pl-[16px] pr-[8px] relative min-w-min border-solid border-x-0 border-y-0 border-transparent bg-white rounded-[40px] opacity-100" 
+                  href="https://hackmd.io/@YN3HXyLVQCqP7Q9KVoCCwQ/r1kxJGiwn/edit" 
+                  target="_blank"
+                >
                   <div className="relative flex-none h-auto select-none whitespace-pre w-auto flex flex-col justify-start shrink-0 color-black tracking-[16px] opacity-100">
                     <p className="text-base font-sans not-italic font-semibold tracking-normal leading-6 text-start decoration-0 normal-case text-[#1A1D1E]">
                       Introducing our On-Chain 2FA
@@ -82,19 +86,25 @@ export default function Main() {
             <div className="flex flex-none flex-col items-center gao-0 h-min justify-start overflow-visible p-0relative w-full opacity-100 " style={{ transform: "perspective(1200px)" }}>
               <div className="flex-none h-auto relative whitespace-pre-wrap w-full break-words outline-0 flex flex-col justify-start shrink-0">
                 <h1 className="font-sans sm:text-[64px] text-[48px] not-italic font-bold tracking-tight sm:leading-[72px] leading-[56px] text-white text-center">Reconnect the Web</h1>
-                <h1 className="font-sans sm:text-[64px] text-[48px] not-italic font-bold tracking-tight sm:leading-[72px] leading-[56px] text-white text-center">with an open, permissions identity layer</h1>
+                <h1 className="font-sans sm:text-[64px] text-[48px] not-italic font-bold tracking-tight sm:leading-[72px] leading-[56px] text-white text-center">
+                  with an open, permissions
+                  <span className="identity-style inline-block bg-clip-text text-transparent">
+                    &nbsp;identity layer
+                  </span>
+                </h1>
               </div>
-              <div className="flex-none h-auto relative whitespace-pre-wrap w-full break-words z-1 outline-0 flex flex-col justify-start shrink-0">
+              {/* <div className="flex-none h-auto relative whitespace-pre-wrap w-full break-words z-1 outline-0 flex flex-col justify-start shrink-0">
                 <div className="font-sans text-[2.5rem] sm:text-[3rem] not-italic font-medium tracking-tight sm:leading-[72px] leading-[56px] text-white text-center">
                   <span style={{ backgroundImage: "linear-gradient(to right bottom, rgb(255, 255, 255) 30%, rgba(255, 255, 255, 0.38))" }} className="inline-block bg-clip-text text-transparent">
                     <span className="text-[#308AF5]">{text}</span>
-                    &nbsp;is your web3 account</span>
+                    &nbsp;is your web3 account
+                  </span>
                 </div>
-              </div>
+              </div> */}
             </div>
             <div className="tracking-[16px] flex-none h-auto relative whitespace-pre-wrap w-full break-word flex flex-col justify-start opacity-100 shrink-0" style={{ transform: "perspective(1200px)" }}>
               <p className="font-sans text-[20px] not-italic font-normal leading-8 tracking-normal text-center text-[#B4BCD0]">
-                Enhance your design system by creating your own source of truth, improve collaboration between design and development and reduce manual work.
+                Enhance the sprit of internet by bringing back the interoperability with a permissionless identity layer secured by blockchain technology.
               </p>
             </div>
             <div className="flex items-center flex-none flex-row flex-nowrap gap-[16px] h-min justify-center overflow-visible p-0 relative w-full opacity-100" style={{ transform: "perspective(1200px)" }}>
@@ -109,11 +119,11 @@ export default function Main() {
               </div>
             </div>
           </div>
-          <div className="relative mt-16 sm:mt-28">
+          <div className="relative mt-16 sm:mt-28 max-w-[1120px]">
             <div className="picture-background absolute m-auto blur-[160px] max-w-3xl h-[250px] top-12 inset-0 sm:h-[300px] lg:h-[650px]"></div>
             <div className="relative">
               {/* <img className="shadow-lg rounded-2xl image-transparent" src="https://eclipsefin.netlify.app/_ipx/w_3840,q_75/%2F_next%2Fstatic%2Fmedia%2Freadme1.f23860ab.png?url=%2F_next%2Fstatic%2Fmedia%2Freadme1.f23860ab.png&w=3840&q=75" width="1920" height="1080" decoding="async" data-nimg="1" loading="lazy" /> */}
-              <img className="shadow-lg rounded-2xl image-transparent"  src={landing} width="1920" height="800" decoding="async" data-nimg="1" loading="lazy" />
+              <img className="shadow-lg rounded-2xl image-transparent" src={landing} width="1920" height="800" decoding="async" data-nimg="1" loading="lazy" />
             </div>
           </div>
         </div>
